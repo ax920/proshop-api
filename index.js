cc6034dfb7866ec862ef667f3613af588dca432a
Alvin Xu <alvin.xu920@gmail.com>
2020-09-16 14:30:14 -0700

    fix get sales db query

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
-require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const app = express();
 const { Client } = require('pg');
 const cors = require('cors')
 const bodyParser = require('body-parser');
-const { doesNotMatch } = require('assert');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,29 +20,11 @@ const client = new Client({
 
 client.connect();
 
-// client.query('select * from items', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   console.log(JSON.stringify(res.rows))
-
-//   client.end();
-// });
-
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('pages/index'))
-
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'content-type');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//   next();
-// });
-
 
 app.get('/test', (req, res) => {
   console.log(process.env.DATABASE_URL);
@@ -54,10 +35,6 @@ app.get('/items', (req, response) => {
 
   client.query(`select * from items`, (err, res) => {
     if (err) throw err;
-    for (let row of res.rows) {
-      // console.log(JSON.stringify(res.rows));
-    }
-    // client.end();
     response.send(res.rows)
   });
 })
@@ -97,26 +74,14 @@ app.put('/items/:id', (req, response) => {
 })
 
 app.get('/sales', (req, response) => {
-  client.query(`select * from items`, (err, res) => {
+  client.query(`select * from sales`, (err, res) => {
     if (err) throw err;
-    for (let row of res.rows) {
-      // console.log(JSON.stringify(res.rows));
-    }
-    // client.end();
     response.send(res.rows)
   });
 })
 
 app.get('/inventory', (req, response) => {
 
-  // client.query('select * from inventory', (err, res) => {
-  //   if (err) throw err;
-  //   for (let row of res.rows) {
-  //     // console.log(JSON.stringify(res.rows));
-  //   }
-  //   // client.end();
-  //   response.send(res.rows)
-  // });
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
